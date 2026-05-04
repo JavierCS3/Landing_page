636409cd9560362746fa30af0a7584ce8427d7db
Javier <javier.castillo239873@potros.itson.edu.mx>
2026-05-04 16:55:24 -0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+tailwind.config = {
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        "surface-container-lowest": "#ffffff",
+        "on-error-container": "#93000a",
+        "on-surface-variant": "#3d4a3d",
+        "surface-container": "#e5eeff",
+        "surface-variant": "#d3e4fe",
+        "on-primary": "#ffffff",
+        "primary": "#006e2f",
+        "primary-container": "#22c55e",
+        "secondary": "#565e74",
+        "background": "#f8f9ff",
+        "on-background": "#0b1c30"
+      }
+    }
+  }
+}
